10dff170c25752df3086e18d754d7092ec4d2620
Dcarolinamorenoc <156273221+Dcarolinamorenoc@users.noreply.github.com>
2024-01-24 12:37:51 -0500

    Ejercicio 4 sobre los kilometros

---
+++ ejercicio4.py
@@ -0,0 +1,5 @@
+metros= float(input("Querido usuario escriba la cantidad de metros que desea convertir a kilometros: "))
+
+kilometros=metros*((1.0)/1000)
+
+print("El resultado de la conversion de metros a kilometros es de ", kilometros)
